734a8b763ed246a9d9691f0965f96ec1cc17485c
Josemcr1134 <cuelloroyopro@gmail.com>
2025-03-17 00:07:11 -0500

    ui updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+    // canActivate:[AuthGuard]
+  },
+  {
+    path:'**',
+    redirectTo:'',
+    pathMatch:'full'
+  }
+];
